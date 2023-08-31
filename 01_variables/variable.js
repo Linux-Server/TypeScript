57c30c781680c344f788fe6bf9e752d9a84e34bd
Sachin6624 <sachin.murali@firstwave.sg>
2023-08-31 23:37:22 +0530

    variables in typescript

---
+++ 01_variables/variable.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+1.create a varibale with let or const or var
+2. all the types should be lowercase
+3. run any tsc file with command:  tsc filename.ts
+4. which will generate a js file with the same name of ts file and that js file will contain the transpiled ts code
+
+
+*/
+exports.__esModule = true;
+//string type
+var greeting = "Sachin"; //annote the type
+var name = "Anju"; // compiler will infer the type based on value
+var hundered = 100;
+var ten = 10.6; // there no float
+console.log(greeting);
---
+++ intro.ts
@@ -0,0 +1,4 @@
+// All the types in the tsc are lowercase
+
+var user = {name:"Sachin", age:28, email:"sam@gmail.com"};
+console.log(user);
