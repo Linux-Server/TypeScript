338cc492b023e35cd0b4cf56fce2ed46843cd0b7
Sachin6624 <sachin.murali@firstwave.sg>
2023-09-01 09:46:07 +0530

    tsc function and any part 1 completed

---
+++ 03_functions/functions.ts
@@ -0,0 +1,24 @@
+
+function add(num: number){
+    return num +10;
+}
+
+function getUpper(str:string){
+    return str.toUpperCase()
+}
+
+function getStatus(status: boolean){
+    return status;
+}
+
+let loginUser = (emali:string, password:string, status:boolean)=>{
+
+}
+
+
+add(22);
+getUpper("sam");
+getStatus(true);
+loginUser("sam","ray", getStatus(true));
+
+export {}
