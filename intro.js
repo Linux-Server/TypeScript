dbed4263c9b870e4cf7cceb130118f5b4d6045db
Sachin6624 <sachin.murali@firstwave.sg>
2023-09-02 19:36:32 +0530

    proper folder tracking

--- intro.js
+++
@@ -1,2 +0,0 @@
-var user = { name: "Sachin", age: 28, email: "sam@gmail.com" };
-console.log(user);
--- intro.ts
+++
@@ -1,4 +0,0 @@
-// All the types in the tsc are lowercase
-
-var user = {name:"Sachin", age:28, email:"sam@gmail.com"};
-console.log(user);
