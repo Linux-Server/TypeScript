c98de201708e0738edb991514f906fdd5714436e
Sachin <sachin6624@gmail.com>
2023-09-04 19:07:19 +0530

    custom types with the help of literal types

---
+++ assertion/assertion.ts
@@ -0,0 +1,16 @@
+//Literal types allow to set custom types
+
+let name:"sachin" = "sachin";
+
+name= "sachin"; //sachin is on only assignable
+
+const chnagDirection = (data: "left" | "right" | "forward")=>{
+     console.log(data);
+}
+
+chnagDirection("left");
+ 
+
+
+
+export{}
