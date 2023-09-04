35856afec6e61890aa3cafc78b1b459b1f6c1bdc
Sachin <sachin6624@gmail.com>
2023-09-04 20:17:18 +0530

    null or undefined

---
+++ null_undefined/null.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+function createPerson(name) {
+    console.log(name.toUpperCase());
+}
+createPerson(null);
---
+++ null_undefined/null.ts
@@ -0,0 +1,13 @@
+function createPerson(name:string | null){
+    /**
+     * TypeScript also has a special syntax for removing null and undefined from a type without doing any explicit checking. Writing ! after any expression is effectively a type assertion that the value isn’t null or undefined:
+
+
+     */
+    console.log(name!.toUpperCase());
+}
+
+
+createPerson(null);
+
+export{}
